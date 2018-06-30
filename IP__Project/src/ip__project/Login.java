8c2029400c3004c72021dafa5f8cdf567b5bf99e
Chinmay-Shivansh <chinmaydalal@rocketmail.com>
2018-06-30 23:13:41 +0530

    Undo Check

--- IP__Project/src/ip__project/Login.java
+++ IP__Project/src/ip__project/Login.java
@@ -71,14 +71,6 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        try
-        {
-            
-        }
-        catch(Exception e)
-        {
-            
-        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
